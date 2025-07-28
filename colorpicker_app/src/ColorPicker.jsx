5ee3f800d0ed2b15e94eb3e70b419ecb4771a47f
Mukunda Mahat <mahatmuku567@gmail.com>
2025-07-28 20:46:01 +0545

    color picker background

--- colorpicker_app/src/ColorPicker.jsx
+++ colorpicker_app/src/ColorPicker.jsx
@@ -2,21 +2,33 @@ import React,{useState} from "react"
 
 function ColorPicker(){
     const [color,setColor] = useState("green");
+    const [bgcolor,setBgcolor]=useState("black")
 
     function handleColorChange(event){
     setColor(event.target.value)
     }
 
-    return(
-        <div className="colorpicker-container">
-            <h1>Color Picker</h1>
-            <div className="color-display" style={{backgroundColor:color}}>
-                <p>Selected Color:{color}</p>
-            </div>
-            <label>Select a Color:</label>
-            <input type="color" value={color} onChange={handleColorChange}/>
+    function handleBgColorChange(event){
+        setBgcolor(event.target.value)
+    }
 
-        </div>
+    return(
+
+        <><body style={{ backgroundColor: bgcolor }}>
+
+        </body>
+        <div className="colorpicker-container">
+                <h1>Color Picker</h1>
+                <div className="color-display" style={{ backgroundColor: color }}>
+                    <p>Selected Color:{color}</p>
+                </div>
+                <label>Select a Color:</label>
+                <input type="color" value={color} onChange={handleColorChange} />
+
+                <label>Select for Background</label>
+                <input type="color" value={bgcolor} onChange={handleBgColorChange} />
+
+            </div></>
     )
 
 
